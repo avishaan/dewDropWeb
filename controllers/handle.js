bc7ed9f936d1010916e739e8b8f894ab61960848
Avishaan <avishaan@codeHatcher.com>
2015-01-17 18:01:20 -0800

    have backend return correct information to route front end

---
+++ controllers/handle.js
@@ -0,0 +1,14 @@
+var querystring = require('querystring');
+var logger = require('./../loggers/logger.js');
+var User = require('./../models/user.js');
+var config = require('../config.js');
+
+module.exports.readHandle = function readHandle (req, res, next) {
+  // get the handle id from the route
+  var hid = req.swagger.params.hid.value;
+  logger.info('Lookup user twitter handle ' + hid);
+  // make sure to unescape any characters
+  pid = querystring.unescape(pid);
+  // make the call to Mark's api here and return data to FE
+  res.status(200).send({test: 'test ok'});
+};
